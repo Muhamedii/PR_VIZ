de7c129ea159a2c8d1c2c124cf328379304d06ae
muhamedzeqirii <muhamed.zeqiri@studentet.uni-pr.edu>
2023-05-22 00:03:22 +0200

    added Clock Page Replacement, added reset functionality in client side and some cleanup

---
+++ src/WebviewAppShared/Data/Services/CprService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using WebviewAppShared.Data.Models;
+
+namespace WebviewAppShared.Data.Services
+{
+    public class CprService : PageReplacementParameters
+    {
+        public HashSet<int> Items { get; set; }
+
+        public (int faults, int hits) Replace(List<int> referenceString, int frameSize)
+        {
+            int pageFaults = 0;
+            int pageHits = 0;
+            int currentIndex = 0;
+
+            foreach (int pageNumber in referenceString)
+            {
+                if (!Items.Contains(pageNumber))
+                {
+                    if (Items.Count == frameSize)
+                    {
+                        while (true)
+                        {
+                            int page = referenceString[currentIndex];
+                            if (!Items.Contains(page))
+                            {
+                                Items.Remove(page);
+                                break;
+                            }
+                            currentIndex = (currentIndex + 1) % referenceString.Count;
+                        }
+                    }
+                    Items.Add(pageNumber);
+                    pageFaults++;
+                }
+                else
+                {
+                    pageHits++;
+                }
+            }
+
+            return (pageFaults, pageHits);
+        }
+
+        public Result GetResult(RequestPayload inputData)
+        {
+            Result result = new() { FrameSizePageFailRatioData = new(), RatioSummary = new() };
+            foreach (var frameSizeOption in inputData.Payload)
+            {
+                Items = new HashSet<int>();
+                FrameSize = frameSizeOption.FrameSize;
+
+                var (faults, hits) = Replace(frameSizeOption.Pages, frameSizeOption.FrameSize);
+
+                result.FrameSizePageFailRatioData.Add(new Tuple<int, int>(faults, FrameSize));
+                result.RatioSummary.TotalFaults = faults;
+                result.RatioSummary.TotalHits = hits;
+            }
+            return result;
+        }
+    }
+}
--- src/WebviewAppShared/Data/Services/PageReplacementService.cs
+++ src/WebviewAppShared/Data/Services/PageReplacementService.cs
@@ -8,12 +8,14 @@ namespace WebviewAppShared.Data.Services
         private readonly LifoService _lifoService;
         private readonly FifoService _fifoService;
         private readonly LruService _lruService;
+        private readonly CprService _cprService;
 
         public PageReplacementService()
         {
             _lifoService = new();
             _fifoService = new();
             _lruService = new();
+            _cprService = new();
         }
 
         public Result GetGeneratedResult(RequestPayload requestPayload)
@@ -24,6 +26,7 @@ namespace WebviewAppShared.Data.Services
                 AlgorithmType.LIFO => _lifoService.GetResult(requestPayload),
                 AlgorithmType.FIFO => _fifoService.GetResult(requestPayload),
                 AlgorithmType.LRU => _lruService.GetResult(requestPayload),
+                AlgorithmType.CPR => _cprService.GetResult(requestPayload),
                 _ => null,
             };
         }
